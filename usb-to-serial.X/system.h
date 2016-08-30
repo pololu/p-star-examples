06ac68f8012636b41336226ffbe145803baa2163
David (Pololu) <dev-david@pololu.com>
2016-08-30 15:11:58 -0700

    usb-to-serial: Got the preprocessing stage to pass by adding system.h and usb_config.h.

---
+++ usb-to-serial.X/system.h
@@ -0,0 +1,4 @@
+#ifndef _SYSTEM_H_
+#define _SYSTEM_H_
+
+#endif
--- usb-to-serial.X/usb_config.h
+++ usb-to-serial.X/usb_config.h
@@ -1,4 +1,11 @@
-#ifndef _USB_CONFIG_H
-#define	_USB_CONFIG_H
+#define USB_SUPPORT_DEVICE
+#define USB_SPEED_OPTION              USB_FULL_SPEED
+#define USB_PING_PONG_MODE            USB_PING_PONG__FULL_PING_PONG
+#define USB_TRANSCEIVER_OPTION        USB_INTERNAL_TRANSCEIVER
+#define USB_PULLUP_OPTION             USB_PULLUP_ENABLE
+#define USB_EP0_BUFF_SIZE             64
+#define CDC_COMM_IN_EP_SIZE           64
+#define USB_MAX_NUM_INT               2
+#define USB_MAX_EP_NUMBER             1
+#define USB_NUM_STRING_DESCRIPTORS    3
 
-#endif
