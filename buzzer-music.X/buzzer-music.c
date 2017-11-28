941e1456ddf6b3510b420af80d34170e67f4bc51
David (Pololu) <dev-david@pololu.com>
2017-11-28 11:05:32 -0800

    buzzer-music.c: Got rest notes working properly.

--- buzzer-music.X/buzzer-music.c
+++ buzzer-music.X/buzzer-music.c
@@ -4,7 +4,7 @@
 
 // The half periods of the notes in octave 0.
 // Spot 12 holds the half period for a rest note (0).
-const uint16_t buzzerHalfPeriodTableOctave0[13] =
+const uint16_t buzzerHalfPeriodTableOctave0[12] =
 {
     54545,  // 0 = A    Half period of A = 1500000/(440/16)
     51484,  // 1 = A#   Half period of A# is smaller by a factor of 2^(1/12)
@@ -18,14 +18,13 @@ const uint16_t buzzerHalfPeriodTableOctave0[13] =
     32432,  // 9 = F#
     30612,  // 10 = G
     28894,  // 11 = G#
-    0,      // Rests are encoded as half periods of 0.
 };
 
 // The timeouts of whole notes in octave 6.  A whole note is 2 s, so this is
 // really just 2 times the frequency.
 // Spot 12 holds the timeout value to use for a rest note; the lower-level
 // library treats it as a 1 kHz note for the purpose of setting timeouts.
-const uint16_t buzzerTimeoutTableOctave6[13] =
+const uint16_t buzzerTimeoutTableOctave6[12] =
 {
     3520,  // 0 = A
     3729,  // 1 = A#
@@ -39,7 +38,6 @@ const uint16_t buzzerTimeoutTableOctave6[13] =
     5920,  // 9 = F#
     6272,  // 10 = G
     6645,  // 11 = G#
-    2000,  // Rests are treated as 1 kHz.
 };
 
 uint8_t buzzerMusicRunning = 0;
@@ -77,7 +75,8 @@ void buzzerMusicService()
 
     uint8_t c;
 
-parse_character:
+parseCharacter:
+
     c = *buzzerMusicPointer++;
 
     // Convert to lower case.
@@ -118,14 +117,26 @@ parse_character:
         buzzerMusicRunning = 0;
         return;
     default:
-        goto parse_character;
+        goto parseCharacter;
     }
 
     uint8_t octave = 4;  // tmphax
-    uint8_t durationNum = 4;  // tmphax
+    uint8_t durationShift = 4;  // tmphax
 
-    uint16_t halfPeriod = buzzerHalfPeriodTableOctave0[note] >> octave;
-    uint16_t timeout = buzzerTimeoutTableOctave6[note] >> (6 - octave + durationNum);
+    uint16_t halfPeriod;
+    uint16_t timeout;
+
+    if (note == 12)
+    {
+        // Rest note
+        halfPeriod = 0;
+        timeout = 2000 >> durationShift;  // TODO: this is too long?
+    }
+    else
+    {
+        halfPeriod = buzzerHalfPeriodTableOctave0[note] >> octave;
+        timeout = buzzerTimeoutTableOctave6[note] >> (6 - octave + durationShift);
+    }
 
     buzzerPlayRawToneNext(halfPeriod, timeout);
 }
